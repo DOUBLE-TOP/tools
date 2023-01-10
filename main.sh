0ea0a1cd11ebbbf5c8b24715cea2fc61ff257c5c
razumv <razumv95@gmail.com>
2023-01-10 12:26:16 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install mc wget curl git htop net-tools unzip jq build-essential ncdu tmux make cmake clang pkg-config libssl-dev -y
+sudo apt install mc wget curl git htop net-tools unzip jq build-essential ncdu tmux make cmake clang pkg-config libssl-dev protobuf-compiler-y
