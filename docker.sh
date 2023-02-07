8be77eb7b1f87887f47d9e882d98d4b101b9dc2b
razumv <razumv95@gmail.com>
2023-02-07 11:50:14 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -52,7 +52,7 @@ install() {
 		echo -e "${C_LGn}Docker installation...${RES}"
 		sudo apt update
 		sudo apt upgrade -y
-		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
+		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release apparmor -y
 		. /etc/*-release
 		wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
