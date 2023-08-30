b8d45a926308ffc2593e0e2e06034700381a393c
razumv <42905535+razumv@users.noreply.github.com>
2023-08-30 18:29:36 +0200

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl https://deb.nodesource.com/setup_16.x | sudo bash
+curl https://deb.nodesource.com/setup_18.x | sudo bash
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
