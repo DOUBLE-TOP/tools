7c02c4c2bc8a047be02b599820de7a92b7edc231
razumv <42905535+razumv@users.noreply.github.com>
2023-08-30 22:22:50 +0200

    Update node.sh

--- node.sh
+++ node.sh
@@ -4,4 +4,4 @@ curl https://deb.nodesource.com/setup_18.x | sudo bash
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install nodejs=16.* yarn build-essential jq git -y
+sudo apt install nodejs=18.* yarn build-essential jq git -y
