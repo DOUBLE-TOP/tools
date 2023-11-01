888c972b697d7a3b9646f2ef85dbf572662e929c
razumv <42905535+razumv@users.noreply.github.com>
2023-11-01 21:52:50 +0100

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-curl https://deb.nodesource.com/setup_20.x | sudo bash
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update
-sudo apt install nodejs=20.* yarn build-essential jq git -y
+# curl https://deb.nodesource.com/setup_20.x | sudo bash
+# curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+# sudo apt update
+# sudo apt install nodejs=20.* yarn build-essential jq git -y
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
