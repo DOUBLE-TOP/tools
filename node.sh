855c7017b8fd53fe2dc2e48f844733d2cd97c19a
razumv <42905535+razumv@users.noreply.github.com>
2023-12-14 16:24:01 +0100

    Update node.sh

--- node.sh
+++ node.sh
@@ -13,4 +13,4 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
-sudo apt-get install nodejs -y
+sudo apt-get install nodejs yarn build-essential jq git -y
