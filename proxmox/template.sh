e8b44291c087a0f59eaa66c452c9b9f560938d9d
razumv <42905535+razumv@users.noreply.github.com>
2024-01-17 18:51:18 +0100

    Update template.sh

--- proxmox/template.sh
+++ proxmox/template.sh
@@ -1,5 +1,11 @@
-qm create 3336 --memory 12048 -vcpus 8 --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci
-qm set 3336 --scsi0 vg1:vm-3336-disk-0,size=30G
+user=user
+password=user_password
+vg_name=my_vg
+disk_name=vm-3336-disk-0
+size=30G
+lvcreate -L 30G -n $disk_name $vg_name
+qm create 3336 --memory 12048 -vcpus 8 --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci 
+qm set 3336 --scsi0 my_vg:$disk_name,size=$size
 qm set 3336 --ide2 local:cloudinit
 qm set 3336 --boot order=scsi0
 qm set 3336 --serial0 socket --vga serial0
