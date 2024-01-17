06ec425bcdd65a30ddce039dd8475efd0b3e1ced
razumv <42905535+razumv@users.noreply.github.com>
2024-01-17 18:27:13 +0100

    Update template.sh

--- proxmox/template.sh
+++ proxmox/template.sh
@@ -1,5 +1,5 @@
 qm create 3336 --memory 12048 -vcpus 8 --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci
-qm set 3336 --scsi0 local:0,import-from=/opt/ubuntu-22.04-minimal-cloudimg-amd64.img
+qm set 3336 --scsi0 vg1:vm-3336-disk-0,size=30G
 qm set 3336 --ide2 local:cloudinit
 qm set 3336 --boot order=scsi0
 qm set 3336 --serial0 socket --vga serial0
