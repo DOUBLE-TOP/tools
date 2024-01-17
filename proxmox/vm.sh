b04d4bdfee0562ce7f799ce89107ac3e37ef6a4f
razumv <42905535+razumv@users.noreply.github.com>
2024-01-17 18:54:23 +0100

    Update vm.sh

--- proxmox/vm.sh
+++ proxmox/vm.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-disk=327748
-memory=12000
+memory=12048
 template_vm_id=3336
 bridge="vmbr0"
 cores=4
 sockets=2
 vcpus=8
+vg_name="my_vg"
+size="30G"
 
 while IFS=';' read -r id name ip gw mac
 do
     [[ "$id" =~ ^#.*$ || -z "$id" ]] && continue 
-
+    disk_name=${name}-0
+    
+    lvcreate -L 30G -n $disk_name $vg_name
+    
     qm clone $template_vm_id $id --name $name
 
-    qm resize $id scsi0 +${disk}G
-
-    qm set $id --ipconfig0 ip=$ip/25,gw=$gw --memory $memory --cores $cores --sockets $sockets --vcpus $vcpus --net0 model=virtio,bridge=$bridge,macaddr=$mac
+    qm set $id --ipconfig0 ip=$ip/25,gw=$gw --memory $memory --cores $cores --sockets $sockets --vcpus $vcpus --net0 model=virtio,bridge=$bridge,macaddr=$mac --scsi0 my_vg:$disk_name,size=$size
 
     qm start $id
 
