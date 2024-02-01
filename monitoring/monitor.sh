d1452091c81ff1b0f000665855c4615f356612ce
razumv <42905535+razumv@users.noreply.github.com>
2024-02-01 18:30:27 +0100

    Update monitor.sh

--- monitoring/monitor.sh
+++ monitoring/monitor.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
-cd $HOME
-if [ ! $OWNER ]; then
-	read -p "Введите свой ник, например телеграмм(без @): " OWNER
-fi
-echo 'Владелец: ' $OWNER
-sleep 1
-if [ ! $NODENAME ]; then
-	read -p "Введите название своего сервера: " NODENAME
-fi
-echo 'Название вашего сервера: ' $NODENAME
-sleep 1
 
-sudo systemctl stop prometheus && systemctl disable prometheus
-sudo systemctl stop node_exporter && systemctl disable node_exporter
+set_environment_variables() {
+    if [ -z "$OWNER" ]; then
+        read -p "Введите свой ник, например телеграмм (без @): " OWNER
+        echo 'Владелец: ' $OWNER
+    fi
+    sleep 1
 
-sudo mkdir /etc/prometheus
+    if [ -z "$NODENAME" ]; then
+        read -p "Введите название своего сервера: " NODENAME
+        echo 'Название вашего сервера: ' $NODENAME
+    fi
+    sleep 1
+}
 
-sudo apt install wget -y
-wget https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v1.63.0/vmutils-amd64-v1.63.0.tar.gz
-tar xvf vmutils-amd64-v1.63.0.tar.gz
-rm -rf vmutils-amd64-v1.63.0.tar.gz
+stop_and_disable_services() {
+    sudo systemctl stop prometheus && systemctl disable prometheus
+    sudo systemctl stop node_exporter && systemctl disable node_exporter
+}
 
-sudo tee <<EOF >/dev/null /etc/prometheus/prometheus.yml
+install_prometheus() {
+    sudo mkdir -p /etc/prometheus
+
+    sudo apt install wget -y
+    wget https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v1.63.0/vmutils-amd64-v1.63.0.tar.gz
+    tar xvf vmutils-amd64-v1.63.0.tar.gz
+    rm -rf vmutils-amd64-v1.63.0.tar.gz
+}
+
+configure_vmagent() {
+    sudo tee <<EOF >/dev/null /etc/prometheus/prometheus.yml
 global:
   scrape_interval: 30s
   evaluation_interval: 30s
@@ -35,14 +43,9 @@ scrape_configs:
       - targets: ["localhost:9100"]
         labels:
           instance: "$(curl -s ipv4.icanhazip.com)"
-    relabel_configs:
-      - source_labels: [instance]
-        regex: '(.+):.*'
-        replacement: '\${1}:9100'
-        target_label: '__address__'
 EOF
 
-sudo tee <<EOF >/dev/null /etc/systemd/system/vmagent.service
+    sudo tee <<EOF >/dev/null /etc/systemd/system/vmagent.service
 [Unit]
   Description=vmagent Monitoring
   Wants=network-online.target
@@ -58,14 +61,16 @@ sudo tee <<EOF >/dev/null /etc/systemd/system/vmagent.service
   WantedBy=multi-user.target
 EOF
 
-sudo systemctl daemon-reload && sudo systemctl enable vmagent && sudo systemctl restart vmagent
+    sudo systemctl daemon-reload && sudo systemctl enable vmagent && sudo systemctl restart vmagent
+}
 
-wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz
-tar xvf node_exporter-1.1.2.linux-amd64.tar.gz
-sudo cp node_exporter-1.1.2.linux-amd64/node_exporter /usr/local/bin
-rm -rf node_exporter-1.1.2.linux-amd64*
+install_node_exporter() {
+    wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz
+    tar xvf node_exporter-1.1.2.linux-amd64.tar.gz
+    sudo cp node_exporter-1.1.2.linux-amd64/node_exporter /usr/local/bin
+    rm -rf node_exporter-1.1.2.linux-amd64*
 
-sudo tee <<EOF >/dev/null /etc/systemd/system/node_exporter.service
+    sudo tee <<EOF >/dev/null /etc/systemd/system/node_exporter.service
 [Unit]
 Description=Node Exporter
 Wants=network-online.target
@@ -78,6 +83,17 @@ ExecStart=/usr/local/bin/node_exporter --web.listen-address=":9100"
 WantedBy=multi-user.target
 EOF
 
-sudo systemctl daemon-reload && sudo systemctl enable node_exporter && sudo systemctl restart node_exporter
+    sudo systemctl daemon-reload && sudo systemctl enable node_exporter && sudo systemctl restart node_exporter
+}
 
-echo "Monitoring Installed"
+main() {
+    cd $HOME
+    set_environment_variables
+    stop_and_disable_services
+    install_prometheus
+    configure_vmagent
+    install_node_exporter
+    echo "Monitoring Installed"
+}
+
+main "$@"
