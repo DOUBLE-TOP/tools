ec1034c547ed0fc995c03d3baaf07e91df3f4cf3
razumv <42905535+razumv@users.noreply.github.com>
2024-02-20 23:10:32 +0100

    Update template.sh

--- proxmox/template.sh
+++ proxmox/template.sh
@@ -1,15 +1,13 @@
 VMID=9000
+apt-get install libguestfs-tools -y
 STORAGE_POOL=$(vgs --noheadings | awk '{print $1}' | tr -d ' ')
 TEMPLATE_NAME="ubuntu-20.04-template"
 ISO_PATH="/var/lib/vz/template/iso/ubuntu-20.04-minimal-cloudimg-amd64.img"
 DISK_SIZE="5"
-
+virt-customize -a $ISO_PATH --install qemu-guest-agent
 qm create $VMID --name $TEMPLATE_NAME --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci 
-
 qm set $VMID --scsi0 $STORAGE_POOL:0,import-from=$ISO_PATH
-
 qm set $VMID --ide2 local:cloudinit
-
 qm set $VMID --boot c --bootdisk scsi0
 qm set $VMID --serial0 socket --vga serial0
 qm template $VMID
