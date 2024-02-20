e72aef12dc2765eb5cc966d7347a17f075b7b008
razumv <42905535+razumv@users.noreply.github.com>
2024-02-20 01:04:09 +0100

    Update node.sh

--- node.sh
+++ node.sh
@@ -9,6 +9,7 @@
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
+rm -f /etc/apt/keyrings/nodesource.gpg
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
