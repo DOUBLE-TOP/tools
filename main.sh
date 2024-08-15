0e8216f157b007069bc0388dd05f45f68f7cb687
ivan.pohosov <pogosovivan@gmail.com>
2024-08-15 22:15:35 +0300

    Update main script

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install mc wget curl git htop netcat net-tools unzip jq build-essential ncdu tmux make cmake clang pkg-config libssl-dev protobuf-compiler bc -y
+sudo apt update -y
+sudo apt install mc wget curl git htop netcat net-tools unzip jq build-essential ncdu tmux make cmake clang pkg-config libssl-dev protobuf-compiler bc lz4 screen -y
