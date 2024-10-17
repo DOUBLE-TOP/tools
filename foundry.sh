ccf1db3af2fef974d3738c0c6b1eedf4ca65a554
ivan.pohosov <pogosovivan@gmail.com>
2024-10-17 03:58:04 +0300

    add foundry install

---
+++ foundry.sh
@@ -0,0 +1,16 @@
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
+echo "Устанавливаем foundry"
+echo "-----------------------------------------------------------------------------"
+
+# Установка Foundry
+cd $HOME
+mkdir -p foundry
+cd foundry
+curl -L https://foundry.paradigm.xyz | bash
+source ~/.bashrc
+echo 'export PATH="$PATH:/root/.foundry/bin"' >> .profile
+source .profile
+
+foundryup
