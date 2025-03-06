7a75ab8459897d2e4767fcc8f3d1d676628c49a9
Oleg <legolassoi@gmail.com>
2025-03-06 11:10:19 +0200

    Update main.sh
    specified netcat version

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt update -y
-sudo apt install mc wget curl git htop netcat net-tools unzip jq build-essential ncdu tmux make cmake clang pkg-config libssl-dev protobuf-compiler bc lz4 screen -y
+sudo apt install mc wget curl git htop netcat-openbsd net-tools unzip jq build-essential ncdu tmux make cmake clang pkg-config libssl-dev protobuf-compiler bc lz4 screen -y
