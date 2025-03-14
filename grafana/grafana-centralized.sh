966439dc84fe879f02b6dc4df617daec806afe91
Oleg <legolassoi@gmail.com>
2025-03-14 13:47:08 +0200

    Update grafana-centralized.sh

--- grafana/grafana-centralized.sh
+++ grafana/grafana-centralized.sh
@@ -19,7 +19,7 @@ echo "Отключили prometheus (если он был)"
 
 sudo apt-get remove prometheus -y &>/dev/null
 sudo rm -rf /etc/prometheus /var/lib/prometheus
-sudo apt-get autoremove -y &>/dev/null
+sudo apt-get autoremove -y
 echo "Удалили prometheus (если он был)"
 
 sudo apt-get update
